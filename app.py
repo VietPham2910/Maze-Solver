57acb22cc0db0bfa0b91dec35f9975b7074a9e61
Viet Pham <phviet2910@gmail.com>
2024-04-25 14:33:23 +0700

    Completed maze solving method

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ from maze import Maze
 
 win = Window(800, 600)
 maze = Maze(20, 20, 10, 10, 50, 50, win=win)
-maze._break_walls_r(0, 0)
-print("Done breaking walls")
+maze.solve()
 win.wait_for_close()
--- maze.py
+++ maze.py
@@ -24,11 +24,14 @@ class Maze:
         self._win:Window = win
         self._cells = []
 
-        self._create_cell()
         if seed:
             random.seed(seed)
+        self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        self._reset_cells_visted()
 
-    def _create_cell(self):
+    def _create_cells(self):
         for _ in range(self._num_rows):
             row = []
             for _ in range(self._num_cols):
@@ -37,16 +40,15 @@ class Maze:
         for i in range(self._num_rows):
             for j in range(self._num_cols):
                 self._draw_cell(i, j)
-        self._break_entrance_and_exit()
 
     def _draw_cell(self, i, j) :
         self._cells[i][j].draw(j*self._cell_size_x + self._x1, 
                                i*self._cell_size_y + self._y1, 
                                (j+1)*self._cell_size_x + self._x1, 
                                (i+1)*self._cell_size_y + self._y1)
-        self._animated()
+        self._animate()
     
-    def _animated(self):
+    def _animate(self):
         self._win.redraw()
         time.sleep(0.05)
 
@@ -88,8 +90,79 @@ class Maze:
                 else:
                     self._cells[i][j].has_bottom_wall = False
                     self._cells[i+1][j].has_top_wall = False
-            self._draw_cell(i, j)
             self._break_walls_r(next_direction[0], next_direction[1])
+
+    def _reset_cells_visted(self):
+        for row in self._cells:
+            for cell in row:
+                cell.visited = False
+    
+    # returns True if this is the end cell, OR if it leads to the end cell.
+    # returns False if this is a loser cell.
+    def _solve_r(self, i, j):
+        self._animate()
+
+        # vist the current cell
+        self._cells[i][j].visited = True
+
+        # if we are at the end cell, we are done!
+        if i == self._num_rows - 1 and j == self._num_cols - 1:
+            return True
+
+        # move left if there is no wall and it hasn't been visited
+        if (
+            j > 0
+            and not self._cells[i][j].has_left_wall
+            and not self._cells[i][j-1].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i][j-1])
+            if self._solve_r(i, j-1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j-1], True)
+
+        # move right if there is no wall and it hasn't been visited
+        if (
+            j < self._num_cols - 1
+            and not self._cells[i][j].has_right_wall
+            and not self._cells[i][j+1].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i][j+1])
+            if self._solve_r(i, j+1):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i][j+1], True)
+
+        # move up if there is no wall and it hasn't been visited
+        if (
+            i > 0
+            and not self._cells[i][j].has_top_wall
+            and not self._cells[i-1][j].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i-1][j])
+            if self._solve_r(i-1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i-1][j], True)
+
+        # move down if there is no wall and it hasn't been visited
+        if (
+            i < self._num_rows - 1
+            and not self._cells[i][j].has_bottom_wall
+            and not self._cells[i+1][j].visited
+        ):
+            self._cells[i][j].draw_move(self._cells[i+1][j])
+            if self._solve_r(i+1, j):
+                return True
+            else:
+                self._cells[i][j].draw_move(self._cells[i+1][j], True)
+
+        # we went the wrong way let the previous cell know by returning False
+        return False
+
+    # create the moves for the solution using a depth first search
+    def solve(self):
+        return self._solve_r(0, 0)
 
             
 
