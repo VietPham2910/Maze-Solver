f45a3a06f53d397bd8ef35a734b51169218ce953
Viet Pham <phviet2910@gmail.com>
2024-04-25 13:58:17 +0700

    Added walls breaking recursive methid

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from graphics import *
 from maze import Maze
 
-win = Window(1200, 800)
-maze = Maze(20, 20, 10, 10, 50, 50, win)
+win = Window(800, 600)
+maze = Maze(20, 20, 10, 10, 50, 50, win=win)
+maze._break_walls_r(0, 0)
+print("Done breaking walls")
 win.wait_for_close()
--- maze.py
+++ maze.py
@@ -1,3 +1,4 @@
+import random
 from cell import Cell
 from graphics import *
 import time
@@ -11,6 +12,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
+        seed=None,
         win=None,
     ):
         self._x1 = x1
@@ -23,6 +25,8 @@ class Maze:
         self._cells = []
 
         self._create_cell()
+        if seed:
+            random.seed(seed)
 
     def _create_cell(self):
         for _ in range(self._num_rows):
@@ -51,3 +55,46 @@ class Maze:
         self._cells[self._num_rows - 1][self._num_cols-1].has_bottom_wall = False
         self._draw_cell(0, 0)
         self._draw_cell(self._num_rows - 1, self._num_cols-1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            adjacent_list = [(i, j-1), (i-1, j), (i, j+1), (i+1, j)]
+            possible_directions = []
+
+            for neighbor in adjacent_list:
+                if (neighbor[0] >= 0 and neighbor[1] >= 0
+                    and neighbor[0] <= self._num_rows - 1 
+                    and neighbor[1] <= self._num_cols - 1
+                    and self._cells[neighbor[0]][neighbor[1]].visited == False
+                ):
+                    possible_directions.append(neighbor)
+            if len(possible_directions) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            next_direction = possible_directions[random.randint(0, len(possible_directions)-1)]
+            if next_direction[0] == i:
+                if next_direction[1] == j - 1:
+                    self._cells[i][j].has_left_wall = False
+                    self._cells[i][j-1].has_right_wall = False
+                else:
+                    self._cells[i][j].has_right_wall = False
+                    self._cells[i][j+1].has_left_wall = False
+            else:
+                if next_direction[0] == i - 1:
+                    self._cells[i][j].has_top_wall = False
+                    self._cells[i-1][j].has_bottom_wall = False
+                else:
+                    self._cells[i][j].has_bottom_wall = False
+                    self._cells[i+1][j].has_top_wall = False
+            self._draw_cell(i, j)
+            self._break_walls_r(next_direction[0], next_direction[1])
+
+            
+
+
+                
+
+
+
